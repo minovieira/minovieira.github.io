e7c4cd1adc70aa26cd1b905150c5405add4a04dd
Mino Vieira <dmino3044@gmail.com>
2021-02-16 01:21:46 +0200

    update2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
   $(window).scroll(function(){
     if(this.scrollY > 20){
@@ -23,30 +22,4 @@ $(document).ready(function(){
     type: 'image',
     gallery:{enabled:true}
   });
-=======
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
-      $(".navbar").addClass("sticky");
-      $(".goTop").fadeIn();
-    }
-    else{
-      $(".navbar").removeClass("sticky");
-      $(".goTop").fadeOut();
-    }
-  });
-
-  $(".goTop").click(function(){scroll(0,0)});
-
-  $('.menu-toggler').click(function(){
-    $(this).toggleClass("active");
-    $(".navbar-menu").toggleClass("active");
-  });
-
-  $(".works").magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery:{enabled:true}
-  });
->>>>>>> d4c6b9b379c93f84816ae94d4f2136aa935f862e
 });
