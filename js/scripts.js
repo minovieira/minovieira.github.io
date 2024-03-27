6c27c4b7489f3adc7e5de54424715d410ea258fa
Mino Vieira <63797830+minovieira@users.noreply.github.com>
2024-03-27 15:42:47 +0100

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,35 @@ $(document).ready(function(){
     gallery:{enabled:true}
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the link element
+    var downloadLink = document.querySelector('.download-link');
+
+    // Add click event listener to the link
+    downloadLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default link behavior
+
+        downloadPDFs();
+    });
+
+    function downloadPDFs() {
+        // Array of PDF file URLs
+        var pdfUrls = [
+            'Qualifications/scan0202.pdf',
+            // 'Qualifications/Portuguese translation of the Secondadry School Certificate.pdf',
+            'path_to_pdf_3.pdf'
+            // Add more PDF URLs as needed
+        ];
+
+        // Iterate through the array and trigger download for each PDF file
+        pdfUrls.forEach(function(url) {
+            var link = document.createElement('a');
+            link.href = url;
+            link.download = url.split('/').pop(); // Set the file name for download
+            link.click();
+        });
+    }
+});
+
